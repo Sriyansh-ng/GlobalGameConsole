c74d47fd3e14b0abeb025001515370a58e7275b4
Sriyansh-ng <sriyansh.nirmal@gmail.com>
2025-09-04 19:51:25 -0700

    Add requirements.txt

--- app.py
+++ app.py
@@ -2,11 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Route for Snake Game
 @app.route("/")
-def snake_game():
+def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    # Run on port 5001 to avoid conflicts with other Flask apps
-    app.run(debug=True, port=5001)
+    # For hosting, do NOT use debug=True; Dont use. Render sets its own environment
+    app.run(host="0.0.0.0", port=5000)
